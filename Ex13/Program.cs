de8e476471deacbc5355cc8ffa27d3bd312fb36e
AndyTkach <andrutka4@gmai.com>
2022-12-22 18:58:08 +0300

    Выполнил всё

---
+++ Ex13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое чилсо: ");
+string userInput = Console.ReadLine() ?? "";
+
+int number = int.Parse(userInput);
+int result = 0;
+
+if (number > 99)
+{
+    result = (number / 100) % 10;
+    Console.WriteLine($"{number} -> {result}");
+}
+else 
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+
